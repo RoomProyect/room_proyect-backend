1bd1cd151db33adc3df08f051c1ac1b68900426f
pabloponc <pablitooponcee@gmail.com>
2024-01-12 13:12:08 -0300

    Controller combined

--- src/controllers/apartmentController.js
+++ src/controllers/apartmentController.js
@@ -7,38 +7,30 @@ const postApartmentController = async (data) => {
   return response;
 };
 
-const getApartmentsControler = async () => {
+const getApartmentsController = async () => {
   const dbApartments = await apartmentSchema.find();
 
   return [...dbApartments];
 };
 
-const getApartamentController = async() => {
-    const apartments = await apartmentSchema.find();
-
-    return apartments;
-}
-
 const getApartamentByLocation = async (country, city) => {
-    const allApartaments = await getApartamentController()
-    const apartamentsByCountry = allApartaments.filter((apartament) => apartament.country.toLowerCase().includes(country.toLowerCase()))
+  const allApartaments = await getApartamentController();
+  const apartamentsByCountry = allApartaments.filter((apartament) =>
+    apartament.country.toLowerCase().includes(country.toLowerCase())
+  );
 
-    // if(!apartamentsByCountry.length) return `Sin datos con el nombre: ${country}`
+  // if(!apartamentsByCountry.length) return `Sin datos con el nombre: ${country}`
 
-    // if(apartamentsByCountry && city) {
-    //     const response = apartamentsByCountry.filter((apartament) => apartament.city.toLowerCase().includes(city.toLowerCase()))
-    //     return response
-    // } else return apartamentsByCountry
-    return apartamentsByCountry
-    // if(!apartamentsByCountry.length) return `Sin datos con el nombre: ${country}`
-
-}
+  // if(apartamentsByCountry && city) {
+  //     const response = apartamentsByCountry.filter((apartament) => apartament.city.toLowerCase().includes(city.toLowerCase()))
+  //     return response
+  // } else return apartamentsByCountry
+  return apartamentsByCountry;
+  // if(!apartamentsByCountry.length) return `Sin datos con el nombre: ${country}`
+};
 
 module.exports = {
-
-    getApartmentsControler,
-    postApartmentController,
-    getApartamentController,
-    getApartamentByLocation
-}
-
+  getApartmentsController,
+  postApartmentController,
+  getApartamentByLocation,
+};
