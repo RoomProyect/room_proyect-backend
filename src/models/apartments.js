12b0c1c635e511f4560c37044c4cb397d48fea03
Facu Sanchez <facusanchz96@gmail.com>
2024-01-14 13:34:48 -0300

    ian puto

--- src/models/apartments.js
+++ src/models/apartments.js
@@ -1,71 +1,27 @@
 const mongoose = require( 'mongoose' );
 
 const apartmentSchema = mongoose.Schema({
-    title: {
+    ambientes: {
         type: String
     },
-    city:{
+    baños:{
         type: String
     },
-    country: {
+    cochera: {
         type: String
     },
-    description: {
+    descripcion: {
         type: String
     },
-    qualification: {
-        type: Number,
-        validate: {
-            validator: function(value) {
-              return value >= 0.00 && value <= 5.00;
-            },
-            message: 'La calificación debe estar entre 0.00 y 5.00'
-        }
+    img: {
+        type: String
     },
-    rooms:{
-        type: Number
+    mcTerreno: {
+        type: String
     },
-    beds:{
-        type: Number
+    precio: {
+        type: String
     },
-    bathrooms: {
-        type: Number
-    },
-    guests: {
-        type: Number
-    },
-    //boolean data
-    wifi:{
-        type: Boolean
-    },
-    parking: {
-        type: Boolean
-    },
-    washing_machine:{
-        type: Boolean
-    },
-    carbon_monoxide_detector:{
-        type: Boolean
-    },
-    kitchen: {
-        type: Boolean
-    },
-    work_zone: {
-        type: Boolean
-    },
-    television: {
-        type: Boolean,
-    },
-    air_conditioning: {
-        type: Boolean
-    },
-    smoke_detector:{
-        type: Boolean
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId, ref: 'User',
-        required: true
-    }
 });
 
 module.exports = mongoose.model( 'Apartment', apartmentSchema );
