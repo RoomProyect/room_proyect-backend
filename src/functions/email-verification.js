82bda117f39b9d95159d37adec58e44f307ec82b
YamilCondori <yamil_condori_@outlook.com>
2024-01-29 21:44:29 -0300

    new email message

--- src/functions/email-verification.js
+++ src/functions/email-verification.js
@@ -34,13 +34,13 @@ const verification_email_message = async (email, name) =>{
                         font-family: Arial, sans-serif;
                         margin: 0;
                         padding: 0;
-                        background-color: #f4f4f4;
+                        background-color: #ffffff;
                     }
             
                     #container {
                         max-width: 600px;
                         margin: 20px auto;
-                        background-color: #ffffff;
+                        background-color: #f4f4f4;
                         padding: 20px;
                         border-radius: 10px;
                         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -65,18 +65,29 @@ const verification_email_message = async (email, name) =>{
                         color: #ffffff;
                         border-radius: 5px;
                     }
+            
+                    .center {
+                        width: 100%;
+                    }
                 </style>
             </head>
             <body>
                 <div id="container">
                     <h1>Bienvenido a Roomvesta</h1>
                     <p>¡Hola ${name}!</p>
-                    <p>Gracias por unirte a la comunidad de Roomvesta, donde puedes comprar terrenos de manera fácil y segura.</p>
+                    <p>Gracias por unirte a la comunidad de Roomvesta, donde puedes comprar y vender terrenos de manera fácil y segura.</p>
                     <p>Estamos emocionados de tenerte a bordo. ¡Comienza a explorar y descubrir las mejores oportunidades!</p>
-                    <a href="https://www.roomvesta.com" class="button">Ir a Roomvesta</a>
+                    <div class="center" style="padding-left: 38%;">
+                        <a href="https://www.roomvesta.com" class="button">Ir a Roomvesta</a>
+                    </div>
                     <p>Si tienes alguna pregunta o necesitas asistencia, no dudes en ponerte en contacto con nuestro equipo de soporte.</p>
                     <p>¡Gracias nuevamente y esperamos que tengas una excelente experiencia en Roomvesta!</p>
                     <p>Atentamente,<br>El equipo de Roomvesta</p>
+                    <div class="center" style="padding-left: 45%;">
+                        <a href="https://www.roomvesta.com">
+                            <img src="./casaicono.png" alt="" style="width: 64px; height: 64px;">
+                        </a>
+                    </div>
                 </div>
             </body>
             </html>`), // html body
